97a81de561f0fb7342ac8d493584d1015ca03a36
Alex <alexrazg06@gmail.com>
2025-03-17 22:41:14 +0200

    Upload files

---
+++ HangmanGame/DatabaseManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public static class DatabaseManager
+    {
+        private static string dbPath = "database.db";
+
+        public static bool TryLogin(string login, string password, out bool isNewUser)
+        {
+            isNewUser = false;
+            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;"))
+            {
+                connection.Open();
+
+                string checkUserQuery = "SELECT password FROM Users WHERE login = @login";
+                using (var command = new SQLiteCommand(checkUserQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@login", login);
+                    var result = command.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        return result.ToString() == password;
+                    }
+                    else
+                    {
+                        isNewUser = true;
+                        string insertUserQuery = "INSERT INTO Users (login, password, points, words, role) VALUES (@login, @password, 0, 0, 'user')";
+                        using (var insertCommand = new SQLiteCommand(insertUserQuery, connection))
+                        {
+                            insertCommand.Parameters.AddWithValue("@login", login);
+                            insertCommand.Parameters.AddWithValue("@password", password);
+                            insertCommand.ExecuteNonQuery();
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ HangmanGame/ProfileForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace HangmanGame
+{
+    public partial class ProfileForm : Form
+    {
+        private string username;
+
+        public ProfileForm(string user)
+        {
+            InitializeComponent();
+            username = user;
+            LoadProfileData();
+        }
+
+        private void LoadProfileData()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=database.db;Version=3;"))
+            {
+                conn.Open();
+                string query = "SELECT points, words, login FROM Users WHERE login = @username";
+                SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", username);
+
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    lblNickname.Text = username;
+                    lblPoints.Text = reader["points"].ToString();
+                    lblSolvedWords.Text = reader["words"].ToString();
+                }
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
